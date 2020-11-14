d4619129093504a5696dcececf96cfeffc7ad1d3
Carl Johan Balck <carl-johan.balck@tactel.se>
2020-11-14 19:13:41 +0100

    minor additions

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using vidly.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Inception" };
+            return View(movie);
+        }
+    }
+}
---
+++ vidly/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace vidly.Models
+{
+    public class Movie
+    {
+        public int Id {get; set;}
+        public String Name { get; set; }
+    }
+}
